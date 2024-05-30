c1b7d2f9806bd94bfd5bcd863e4b64fdcfa9b35b
ayecue <soerenwehmeier@googlemail.com>
2024-05-31 01:03:26 +0200

    0.5.0 fix tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,6 @@
 const { miniscriptMeta } = require('../dist');
 
 describe('miniscriptMeta', () => {
-  let meta = null;
-
   test('should return signatures', () => {
     expect(miniscriptMeta.getSignaturesByType('general')).not.toBeNull();
   });
