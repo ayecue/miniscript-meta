28f8d7c20f1e8ad1a741c5f707526e3e77114cf7
ayecue <soerenwehmeier@googlemail.com>
2024-06-02 19:51:17 +0200

    1.0.0 update to new meta-utils

--- src/descriptions/en/index.ts
+++ src/descriptions/en/index.ts
@@ -1,4 +1,4 @@
-import { Descriptions } from 'meta-utils';
+import { DescriptionsPayload } from 'meta-utils';
 
 import Any from './any.json';
 import Generic from './general.json';
@@ -7,7 +7,7 @@ import MapLanguage from './map.json';
 import Number from './number.json';
 import String from './string.json';
 
-const descriptions: Descriptions = {
+const descriptions: DescriptionsPayload = {
   any: Any,
   general: Generic,
   list: List,
--- tests/index.test.js
+++ tests/index.test.js
@@ -2,6 +2,16 @@ const { miniscriptMeta } = require('../dist');
 
 describe('miniscriptMeta', () => {
   test('should return signatures', () => {
-    expect(miniscriptMeta.getSignaturesByType('general')).not.toBeNull();
+    expect(miniscriptMeta.getTypeSignature('general')).not.toBeNull();
+  });
+
+  test('should return multiple matches', () => {
+    const matches = miniscriptMeta.searchDefinitionMatches(['map', 'string', 'list'], 'hasIndex');
+    expect([...matches.keys()]).toEqual(['map', 'string', 'list']);
+  });
+
+  test('should return parent match', () => {
+    const matches = miniscriptMeta.searchDefinitionMatches('version', 'hasIndex');
+    expect([...matches.keys()]).toEqual(['map']);
   });
 });
